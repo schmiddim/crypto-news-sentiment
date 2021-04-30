c18557a31f077dcf657c11d7bb0733e7551873ea
Michael Schmitt <schmiddim@gmx.at>
2021-04-30 18:22:19 +0200

    Extractor for biz, local db setup

--- collect-4chan-biz-data.py
+++ collect-4chan-biz-data.py
@@ -12,18 +12,22 @@ b = basc_py4chan.Board('biz')
 for thread_id in b.get_all_thread_ids():
     thread = b.get_thread(thread_id)
     ids = b.get_all_thread_ids()
-    replies = [{'comment': thread.topic.text_comment, 'date': thread.topic.datetime}]
+    try:
+        replies = [{'comment': thread.topic.text_comment, 'date': thread.topic.datetime}]
 
-    for reply in thread.replies:  # type: basc_py4chan.Post
-        # print(reply.text_comment, reply.datetime)
+        for reply in thread.replies:  # type: basc_py4chan.Post
+            # print(reply.text_comment, reply.datetime)
 
-        replies.append({'comment': reply.text_comment, 'date': reply.datetime})
+            replies.append({'comment': reply.text_comment, 'date': reply.datetime})
 
-    json_data = json.dumps(replies, default=str)
-    sql = """REPLACE   INTO threads 
-                (date_modified, date_created_4chan,date_last_modified_4chan, thread_id, topic, replies) 
-                VALUES (current_timestamp(), %s, %s,%s,%s, %s);
-        """
-    cur = conn.cursor()
-    cur.execute(sql, [thread.topic.datetime, replies[-1].get("date"), thread_id, thread.topic.text_comment, json_data])
-    conn.commit()
+        json_data = json.dumps(replies, default=str)
+        sql = """REPLACE   INTO threads 
+                    (date_modified, date_created_4chan,date_last_modified_4chan, thread_id, topic, replies) 
+                    VALUES (current_timestamp(), %s, %s,%s,%s, %s);
+            """
+        cur = conn.cursor()
+        cur.execute(sql, [thread.topic.datetime, replies[-1].get("date"), thread_id, thread.topic.text_comment, json_data])
+        conn.commit()
+    except Exception as e:
+        print("thread id  {}".format(thread_id))
+        print(e)
